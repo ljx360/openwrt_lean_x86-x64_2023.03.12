915724937cd480aa40d771d9a53cb89cd8b86a5c
ljx360 <79862310+ljx360@users.noreply.github.com>
2023-03-12 09:08:37 +0800

    Create ljx_down_config_by_url.sh

---
+++ ljx_down_config_by_url.sh
@@ -0,0 +1,42 @@
+
+
+ARG_COUNT=$#
+
+if [ $ARG_COUNT -eq 2 ];then
+echo ""
+echo "${ARG_COUNT} IS NOT 2"
+exit
+fi
+
+
+CONFIG_FILE=/workdir/openwrt/.config
+URL=$1
+EXT_CONFIG_FILE=$2
+
+
+
+
+
+if [ ! -e $CONFIG_FILE ];then
+
+
+echo ""
+echo "try down [${URL}] to [${EXT_CONFIG_FILE}]"
+wget -d -O $EXT_CONFIG_FILE $URL
+
+if [ -e $EXT_CONFIG_FILE ];then
+echo ""
+echo "copy $EXT_CONFIG_FILE TO $CONFIG_FILE"
+cp $EXT_CONFIG_FILE $CONFIG_FILE
+else
+echo ""
+echo "MISS $EXT_CONFIG_FILE"
+fi
+
+
+else
+
+echo ""
+echo "EXIST $CONFIG_FILE"
+
+fi
